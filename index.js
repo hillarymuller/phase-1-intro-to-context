20dec94b11322e1fba2a9d471d754cd14eaec697
Hillary Muller <hillaryaward@gmail.com>
2022-06-21 13:58:54 -0500

    commit tree 0dc580472133fed37c9986fa1dcbb52f28cd55aa

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]){
+    return {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees){
+    createEmployeeRecord.call(employees)
+}
